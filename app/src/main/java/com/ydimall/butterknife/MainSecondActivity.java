46c4cb0cbdeb0e5c482cedfb0f6688c046c6973d
liuzhiming <267252782@qq.com>
2018-09-28 14:42:06 +0800

    merge Liu into master

--- app/src/main/java/com/ydimall/butterknife/MainSecondActivity.java
+++ app/src/main/java/com/ydimall/butterknife/MainSecondActivity.java
@@ -107,6 +107,7 @@ public class MainSecondActivity extends AppCompatActivity implements CommandCall
     @Override
     protected void onRestart() {
         super.onRestart();
+        //合并Liu
         Log.d("TAG","onRestart");
         Log.d("TAG","onRestart2");
     }
